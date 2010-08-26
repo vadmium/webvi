a807f0f034670f461bf99345b8fe02cc32ebfd50
Antti Ajanki <antti.ajanki@iki.fi>
2010-08-26 20:49:36 +0300

    update version number

--- src/vdr-plugin/webvideo.c
+++ src/vdr-plugin/webvideo.c
@@ -24,7 +24,7 @@
 #include "common.h"
 #include "timer.h"
 
-const char *VERSION               = "0.3.1";
+const char *VERSION               = "0.3.2";
 static const char *DESCRIPTION    = trNOOP("Download video files from the web");
 static const char *MAINMENUENTRY  = "Webvideo";
 cMimeTypes *MimeTypes             = NULL;
